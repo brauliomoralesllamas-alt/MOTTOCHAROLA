73865523a5dca90c4debfce1d6089b615fc29b69
brauliomoralesllamas-alt <braulio.morales.llamas@gmail.com>
2026-04-29 12:17:49 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+    const MOTTO_CONFIG = {
+        // ESTADO DE LA TIENDA
+        isSoldOut: false,          // Cambia a true si te quedas sin inventario (bloquea la página)
+
+        // HORARIOS DE OPERACIÓN (Formato 24 hrs)
+        startHour: 23,             // 6:00 PM
+        endHour: 24,               // 8:00 PM
+
+        // PRECIOS Y DESCUENTOS
+        precioBowl: 800,           // Precio normal del bowl
+        descuentoBienvenida: 70,   // Descuento en pesos para clientes nuevos
+
+        // CONEXIÓN A BASE DE DATOS
+        scriptURL: 'https://script.google.com/macros/s/AKfycbw6OPIj8OqZQ_fhgrL8-kyEHn9V0wmrHzUNP3xOvEA6QJxS4QrAKySt7Z1Z_2MoowSsRw/exec'
+    };
